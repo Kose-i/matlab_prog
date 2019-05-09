f8517a8d2278c9f57c37ebc453b4b466d2b25fb0
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-09 16:21:34 +0900

    Add tukan28th [1]

--- tukanTest28.m
+++ tukanTest28.m
@@ -1,3 +1,14 @@
+% Problem [1]
+
+figure(1)
+num = [1];
+den = [1, 2, 2];
+K = 0:0.01:1000;
+r = rlocus(num, den, K);
+plot(r, '-');v = [-5, 5, -5, 5];axis(v);axis('square')
+
+% Problem [2]
+figure(2)
 num = [1];
 den = [1, 6, 8, 0];
 K = 0:0.01:1000;
