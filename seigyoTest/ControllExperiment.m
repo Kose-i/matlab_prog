5d112709db8a11b37f2aa89233d8c8925bae3e71
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-06-13 14:29:30 +0900

    Add readcsv and plot

--- seigyoTest/ControllExperiment.m
+++ seigyoTest/ControllExperiment.m
@@ -1,10 +1,18 @@
-num = [1];
-den = [1, 4, 4];
-t = 0:10:1;
-sys = tf(num, den);
-[y,t] = step(sys);
-plot(t, y);
-grid
-xlabel('xlabel')
-ylabel('ylabel')
+%num = [1];
+%den = [1, 4, 4];
+%t = 0:10:1;
+%sys = tf(num, den);
+%[y,t] = step(sys);
+%plot(t, y);
+%grid
+%xlabel('xlabel')
+%ylabel('ylabel')
 % ***step(num, den, t); %
+
+filename = "2019052804.csv";
+m = csvread(filename);
+m
+plot(m(:, 1), m(:,2));
+grid
+ylabel('水位[cm]')
+xlabel('ステップ数')
