d4753c7c32fc955b06f3c20e72e4ed970d1e8934
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-11-01 13:39:55 +0900

    Add graphEdge

--- SeigyoExp3/plotData.py
+++ SeigyoExp3/plotData.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import rcParams
+
+#
+import japanize_matplotlib
+#
+
 rcParams['xtick.direction'] = 'in'
 rcParams['ytick.direction'] = 'in'
 
@@ -11,7 +16,7 @@ rcParams['ytick.direction'] = 'in'
 data_num  = 4 # 使用するデータ番号0~4
 start_pos = 20  # 傾きの始まる点
 end_pos   = 200 # 傾きの終わる点
-sampling_ratio = 29 # サンプリングの周期
+sampling_ratio = 30 # サンプリングの周期
 method_n = 1 # 前nと後nの平均値を取る # 1以外については未実装
 
 
@@ -55,28 +60,42 @@ def error_sum(data, t_data, K_p=2.4908, T_p=1516.3, L_p=23.0744):
     sum_diff = sum([abs(e-f)/e for e,f in zip(data, c)])/len(data)
     return sum_diff
 
-#
-#mat = np.array(csv_read('datasets/bc_controll.CSV'))*100
-#time_mat = np.array(range(0, len(mat)))*10
-#data1 = mat[:,data_num]
-#plt.plot(time_mat, data1)
-#plt.xlabel("t [s]")
-#plt.xticks(range(0,4000,1000))
-#plt.yticks(range(20,80,10))
-#plt.ylabel("Θ [℃]")
-#plt.show()
+# Control_Exp.png
+mat = np.array(csv_read('datasets/bc_controll.CSV'))*100
+time_mat = np.array(range(0, len(mat)))*10
+data1 = mat[:,data_num]
+y = 0*time_mat+59.15
+plt.plot(time_mat, data1)
+plt.plot(time_mat, y, linestyle="dashed")
+plt.xlabel("t [s]")
+plt.xticks(range(0,4000,1000))
+plt.yticks(range(20,80,10))
+#plt.axis(xmin=0, xmax, ymin, ymax)
+plt.axis(xmin=0, xmax=2300)
+plt.ylabel("Θ [℃]")
+tmp_x = np.array(range(1400, 1600, 100))
+tmp_y = 0*tmp_x + 30
+plt.plot(tmp_x, tmp_y)
+tmp_y = 0*tmp_x + 27
+plt.plot(tmp_x, tmp_y, linestyle="dashed")
+plt.text(x=1530, y=30.5-1, s="実測値")
+plt.text(x=1530, y=27.5-1, s="Θ(t)=59.15")
+plt.show()
 #
 mat = np.array(csv_read('datasets/bc.CSV'))*100
 time_mat = np.array(range(0, len(mat)))*10
 #print(mat)
 #print(len(mat))
 #print(len(mat[0]))
+
 data1 = mat[:,data_num] #TODO DATA番号
+## Identification.png
 #plt.plot(time_mat, data1)
 #plt.xlabel("t [s]")
 #plt.xticks(range(0,8000,1000))
 #plt.yticks(range(0,170,10))
 #plt.ylabel("Θ [℃]")
+#plt.axis(xmin=0, xmax=7500)
 #plt.show()
 #print(len(data1))
 #print(len(data1[0]))
@@ -114,21 +133,22 @@ data_diff = next_sample_data - prev_sample_data
 #plt.plot(data_diff)
 log_data = np.log(data_diff)
 
-plt.plot(sample_time,log_data)
 a,b = a_b(mat_x=sample_time, mat_y=log_data)
-x = np.linspace(10*start_pos,end_pos*10-sampling_ratio*10,10)
-y = a*x + b 
-plt.plot(x, y, linestyle="dashed")
-
-# 軸ラベル
-tmp_x = np.array(range(100, 200, 10))
-tmp_y = 0*tmp_x + 2
-plt.plot(tmp_x, tmp_y)
-tmp_x = np.array(range(100, 200, 10))
-tmp_y = 0*tmp_x + 1.9
-plt.plot(tmp_x, tmp_y, linestyle="dashed")
-plt.text(x=220, y=2.00-0.01, s="実測値")
-plt.text(x=220, y=1.90-0.01, s="y=ax+b")
+##z-t200s.png
+#x = np.linspace(10*start_pos,end_pos*10-sampling_ratio*10,10)
+#y = a*x + b 
+#plt.plot(x,y,linestyle="dashed")
+#plt.plot(sample_time,log_data)
+#tmp_x = np.array(range(230, 330, 10))
+#tmp_y = 0*tmp_x + 1.9
+#plt.plot(tmp_x, tmp_y)
+#tmp_x = np.array(range(230, 330, 10))
+#tmp_y = 0*tmp_x + 1.8
+#plt.plot(tmp_x, tmp_y, linestyle="dashed")
+#plt.text(x=350, y=1.90-0.01, s="実測値")
+#plt.text(x=350, y=1.80-0.01, s="z="+str(a)+"t+"+str(b))
+#plt.axis(xmin=190, xmax=end_pos*10-sampling_ratio*10 + 10)
+#plt.show()
 
 print("a:", a, ", b:",b)
 #print("end:", mat[-1, data_num], "start:",mat[0, data_num])
@@ -139,11 +159,24 @@ print("T_p:",T_p)
 L_p = T_p*(b-np.log(50*K_p*(1-np.exp(-sampling_ratio*10/T_p))))
 print("L_p:", L_p)
 
-plt.xlabel("t[s]")
-plt.ylabel("z(t)")
-#plt.xticks(range(0))
-plt.yticks([1.8,2.0,2.2,2.4,2.6,2.8,2.8, 3.0, 3.2])
-plt.show()
+## step_response200s.png
+#model_data = 50*K_p*(1-np.exp(-(time_mat-L_p)/T_p))+data1[0]
+#plt.xlabel("t [s]")
+#plt.ylabel("Θ (t)")
+#plt.plot(time_mat, model_data, linestyle='dashed')
+#plt.plot(time_mat, data1)
+##plt.xticks(range(0))
+## 軸ラベル
+#tmp_x = np.array(range(4000, 5000, 100))
+#tmp_y = 0*tmp_x + 50
+#plt.plot(tmp_x, tmp_y)
+#tmp_y = 0*tmp_x + 40
+#plt.plot(tmp_x, tmp_y, linestyle="dashed")
+#plt.text(x=5100, y=50-1, s="実測値")
+#plt.text(x=5100, y=40-1, s="モデル値")
+#plt.yticks(range(0, 180, 20))
+#plt.axis(xmin=0, xmax=7500)
+#plt.show()
 
 error_t = error_sum(data1, time_mat ,K_p=K_p, T_p=T_p, L_p=L_p)
 #error_t = error_sum(data1, time_mat)
